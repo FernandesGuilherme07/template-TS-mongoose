11a04386b95532e5133e8f654639c3a80c6006e2
Guilherme Fernandes <72757771+FernandesGuilherme07@users.noreply.github.com>
2022-05-19 22:59:53 -0300

    fix production script issues

--- config/default.ts
+++ config/default.ts
@@ -5,16 +5,24 @@ const {
   DB_HOSTNAME,
   DB_PORT,
   APP_PORT,
+  APP_URL,
   ENVIRONMENT,
   // DB_ATLAS
 } = process.env;
+
+const prodOrDev: String =
+  ENVIRONMENT === "development"
+    ? `mongodb://${APP_URL}:${DB_PORT}/${DB_DATABASE}`
+    : `mongodb://${DB_HOSTNAME}:${DB_PORT}/${DB_DATABASE}`;
 
 export default {
   // with MongoDB atlas:
   // dbUri: DB_ATLAS
 
   // with Docker-compose:
-  dbUri: `mongodb://${DB_HOSTNAME}:${DB_PORT}/${DB_DATABASE}`,
+  dbUri: prodOrDev,
+
+  // Development
   env: ENVIRONMENT,
   port: APP_PORT,
 };
